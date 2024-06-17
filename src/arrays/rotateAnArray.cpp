27976e158373815e324587b5f253b952ceebaa90
Manoj Mattur R <86703267+ManojMatturR@users.noreply.github.com>
2024-06-17 17:06:08 +0530

    Create rotateAnArray.cpp

---
+++ src/arrays/rotateAnArray.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+// Write a program to remove duplicates from a sorted array.
+
+using namespace std;
+
+void rotateArray(int arr[], int d, int n) {
+    int temp[d];
+    for (int i = 0; i < d; i++) {
+        temp[i] = arr[i];
+    }
+    for (int i = d; i < n; i++) {
+        arr[i - d] = arr[i];
+    }
+    for (int i = 0; i < d; i++) {
+        arr[n - d + i] = temp[i];
+    }
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5, 6, 7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int d = 2;
+    rotateArray(arr, d, n);
+    cout << "Rotated array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
